f32969f00fe4416a87bbfc2263fa1b88412c1a36
MikaelaMacari <mikaela.malanici@gmail.com>
2023-03-12 10:03:34 +0200

    Created project with create-react-app command and typescript template.Deleted unused files.Added tailwind to this project.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./index.css"
+
+const App: React.FC = () => {
+  return (
+      <h1 className="text-3xl font-bold">
+        Hello world!
+      </h1>
+  );
+}
+
+export default App;
