1c2871801f3cbc17c6dd06ed27278320e301dc78
MikaelaMacari <mikaela.malanici@gmail.com>
2023-03-13 10:50:36 +0200

    Added logic for changing color of pressed key and implemented functionality from number 3.Started to work on nr.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import "./index.css"
 import Keyboard from "components/modules/Keyboard";
 
 const App: React.FC = () => {
+    const [time, setTime] = useState(60)
+    const timeRef = useRef<any>(null);
+    timeRef.current = time;
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if (timeRef?.current > 0) {
+                setTime(prevTime => prevTime - 1);
+            } else {
+                clearInterval(timer);
+            }
+        }, 1000);
+
+    }, [])
+
     return (
         <div className='grid h-screen place-items-center'>
+            <div
+                className='bg-red-900 rounded-full text-white text-4xl h-24 w-24 flex items-center justify-center'>{time}
+            </div>
             <Keyboard/>
         </div>
     );
--- src/components/common/Key/index.tsx
+++ src/components/common/Key/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {icons} from "assets/static/icons";
 import {ItemInterface} from "ts/interfaces/item";
 
@@ -8,13 +8,34 @@ type KeyPropsType = {
 const Key = ({itemKey}: KeyPropsType) => {
     // @ts-ignore
     const Icon = icons[itemKey?.icon]
+    const [keyColor, setKeyColor] = useState<string>('text-gray-500')
+    const handleKeyupColor = (e: any) => {
+        setTimeout(() => {
+            setKeyColor('text-green-500')
+        }, 3000)
+    }
+    const handleKeyDownColor = (event: any) => {
+        setKeyColor('text-sky-500')
+    }
 
+    useEffect(() => {
+        if (keyColor !== 'text-gray-500') {
+            setTimeout(() => {
+                setKeyColor('text-gray-500')
+            }, 6000)
+        }
+    }, [keyColor])
+    
     return (
         <div
-            className={`bg-white ${itemKey?.width || 'w-16'} ${itemKey?.height || 'h-16'} flex flex-col justify-center ${itemKey?.place || 'items-center'} ${itemKey?.place && 'px-3'} rounded-md`}>
+            tabIndex={-1}
+            className={`bg-white ${itemKey?.width || 'w-16'} ${itemKey?.height || 'h-16'} flex flex-col justify-center ${itemKey?.place || 'items-center'} ${itemKey?.place && 'px-3'} rounded-md`}
+            onKeyUp={handleKeyupColor}
+            onKeyDown={handleKeyDownColor}
+        >
             {itemKey?.icon && <Icon color={'disabled'} fontSize={'large'}/>}
-            <p className={`${itemKey?.fontSize || 'text-xl'} text-gray-500`}>{itemKey?.name}</p>
-            <p className={`${itemKey?.fontSize || 'text-xl'} text-gray-500`}>{itemKey?.secondaryName}</p>
+            <p className={`${itemKey?.fontSize || 'text-xl'} ${keyColor}`}>{itemKey?.name}</p>
+            <p className={`${itemKey?.fontSize || 'text-xl'} ${keyColor}`}>{itemKey?.secondaryName}</p>
         </div>
     )
 }
